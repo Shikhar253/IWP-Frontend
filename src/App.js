7781b02cd88674c4f7384b0cb76263ec103aea20
Shikhar253 <saxenashikhar01@gmail.com>
2021-10-12 02:39:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Homepage from './components/Homepage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is Homepage</h1>
-    </div>
+    <>
+    <Homepage/>
+    </>
   );
 }
 
--- src/Components/header.js
+++ src/Components/header.js
@@ -5,12 +5,13 @@ import { LinkContainer } from "react-router-bootstrap";
 const header = () => {
   return (
     <Navbar bg="primary" variant="dark" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand>React-Bootstrap</Navbar.Brand>
-      </LinkContainer>
+        <Navbar.Brand>IWP-PROJECT</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
+        <LinkContainer to="/">
+            <Nav.Link>Home</Nav.Link>
+          </LinkContainer>
         <LinkContainer to="/interview">
             <Nav.Link>Interview</Nav.Link>
           </LinkContainer>
