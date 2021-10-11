1b0ebfb4838ebc750bb047a905c715ac21970e5e
Shikhar253 <saxenashikhar01@gmail.com>
2021-10-12 01:04:34 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import Roadmap from "./components/Roadmap";
+import Interview from "./components/Interview";
+import Updates from "./components/Updates";
 function App() {
   return (
     <>
       <Navbar />
       <Switch>
         <Route path="/" component={Homepage} exact />
-        
+        <Route path="/roadmap" component={Roadmap} exact />
+        <Route path="/interview" component={Interview} exact />
+        <Route path="/updates" component={Updates} exact />
+
         <Route component={Error} />
       </Switch>
     </>
